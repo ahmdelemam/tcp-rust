0242c02e274ea63eddd3aec4083931df078e7a67
ahmdelemam <ahmdelemam@gmail.com>
2024-06-18 23:04:12 +0200

    add tun_tap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+
+    eprint!("Read {} bytes: {:02x?}\n", nbytes, &buf[..nbytes]);
+    Ok(())
 }
+
+ // sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/tcp-rust
+ 
